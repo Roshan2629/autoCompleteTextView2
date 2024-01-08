3e7f982fd59faec2592d0c266a270a670f361c65
lenovo <vinayakgilbile683@gmail.com>
2024-01-08 12:33:49 +0530

    commit

---
+++ app/src/main/java/com/example/autocompletetextview2/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.autocompletetextview2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    String[] subject={"Android","Python","PHP","ETI","MAN"};
+    AutoCompleteTextView actv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ArrayAdapter<String> adapter=new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line,subject);
+        actv=findViewById(R.id.text);
+        actv.setThreshold(1);
+        actv.setAdapter(adapter);
+        actv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this,adapter.getItem(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
